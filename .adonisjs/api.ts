53a6a2f72823a04f733d4e3f65699f0ca057899b
Fawwaz <fawwaz-1427@hotmail.com>
2024-10-19 07:44:55 +0800

    added tuyay

---
+++ .adonisjs/api.ts
@@ -0,0 +1,11 @@
+import type { MakeTuyauRequest, MakeTuyauResponse } from '@tuyau/utils/types'
+import type { InferInput } from '@vinejs/vine/types'
+
+export interface ApiDefinition {
+}
+const routes = [
+] as const;
+export const api = {
+  routes,
+  definition: {} as ApiDefinition
+}
